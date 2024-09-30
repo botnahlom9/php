bcf886b7d69890114e02b7f8fe552b1b63135978
botnahlom9 <botnahlom9@outlook.com>
2024-09-30 17:04:29 +0000

    Update main.php

--- test-project/main.php
+++ test-project/main.php
@@ -1,13 +1,1 @@
-<?php
-/*-------------------------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *-------------------------------------------------------------------------------------------------------------*/
-
-function hello($name) {
-	echo "Hello $name!";
-}
-
-hello('remote world');
-
-?>
+Helo
